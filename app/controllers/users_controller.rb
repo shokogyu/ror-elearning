1f2a4bc74f17eabb44373cc0f5c8c9db8464627f
Shoko Okamoto <mffnoissm23@gmail.com>
2018-05-09 11:59:14 +0800

    Profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
+      flash[:success] = "User created!"
       redirect_to @user
     else
       render 'new'
@@ -20,7 +21,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 
 
   private
